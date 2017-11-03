6d9b983cd0820bc36192978c66b7f49361e7449e
Vitor buxbaum <vitorbuxbaum@gmail.com>
2017-11-03 17:10:00 -0200

    function to return adress of i_bmaps and inode tables

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -1,4 +1,5 @@
 #include "dcc_os_fsck.h"
+#include "list.h"
 
 int block_size;
 int group_size;
@@ -35,15 +36,16 @@ retrieve_sb_backup(const char* file_path) {
 		block_size <<= 1;  
 	}
 	
-	/*printf("Reading super-block from device :\n Inodes count: %u \n Blocks count : %u \n Reserved blocks count   : %u \n Free blocks count: %u\n Creator OS : %u \n First non-reserved inode: %u \n Size of inode structure : %hu \n",
+	printf("Reading super-block from device :\n Inodes count: %u \n Blocks count : %u \n Reserved blocks count   : %u \n Free blocks count: %u\n Creator OS : %u \n First non-reserved inode: %u \n Size of inode structure : %hu \n Inodes per group : %u \n",
 		   super->s_inodes_count,  
 	       super->s_blocks_count,
 	       super->s_r_blocks_count,     // reserved blocks count 
 	       super->s_free_blocks_count,
 	       super->s_creator_os,
 	       super->s_first_ino,          // first non-reserved inode 
-	       super->s_inode_size);
- 	*/
+	       super->s_inode_size,
+	       super->s_inodes_per_group);
+
 	close(img_fd);
 
 	return super;
@@ -68,12 +70,12 @@ fix_superblock(const char *file_path, struct ext2_super_block *sb_backup) {
 
 
 /*
- * Passeia pelos descritores de grupo para tentar recuperar os endereços 
+ * Passeia pelos descritores de grupo para recuperar os endereços 
  * das tabelas de inodes. 
  */
 
-int* 
-find_inode_tables(const char *file_path, int group_count){
+int** 
+find_inode_paths(const char *file_path, int group_count){
 	int img_fd;
 	if ((img_fd = open(file_path, O_RDONLY)) < 0){
         printf("Falha ao Abrir o arquivo %s.\n", file_path);
@@ -82,23 +84,35 @@ find_inode_tables(const char *file_path, int group_count){
 
     struct ext2_group_desc gd;
 	//int *gd_blocks = malloc(group_count * sizeof(int));
-	int *inode_tables = malloc(group_count * sizeof(int));
+
+	int **inode_paths = malloc(2 * sizeof(int*));
+	inode_paths[0] = malloc(group_count * sizeof(int)); // inode bitmaps
+	inode_paths[1] = malloc(group_count * sizeof(int)); // inode tables
 
     for (int i = 0; i < group_count; ++i)
 	{
 		lseek(img_fd, GROUP_OFFSET(i) + block_size, SEEK_SET); // vai ao início do grupo e pula a cópia do superblock
 		read(img_fd, &gd, sizeof(struct ext2_group_desc)); // lê o descritor do grupo
-		inode_tables[i] = gd.bg_inode_table; // adiciona o endereço da tabela de inodes ao vetor
+		
+		if (gd.bg_block_bitmap != 0)
+		{
+			inode_paths[0][i] = gd.bg_inode_bitmap; // adiciona o endereço da bitmap de inodes ao vetor
+			inode_paths[1][i] = gd.bg_inode_table; // adiciona o endereço do tabela de inodes ao vetor
+		} else {
+			inode_paths[0][i] = -1;
+			inode_paths[1][i] = -1;
+		}
+
 		
 		// printf pra ter noção do que tá rolando :P
-		printf("%i %i %i %i\n", gd.bg_block_bitmap, gd.bg_inode_bitmap, gd.bg_inode_table, gd.bg_free_blocks_count);
+		printf("%i %i\n", inode_paths[0][i], inode_paths[1][i]);
 	}
     
-    return inode_tables;
+    return inode_paths;
 }
 
 int*
-check_inodes(const char *file_path, int *inode_tables, int group_count){
+check_inodes(const char *file_path, int *inode_paths, int group_count){
 	int img_fd;
 	if ((img_fd = open(file_path, O_RDONLY)) < 0){
         printf("Falha ao Abrir o arquivo %s.\n", file_path);
@@ -116,6 +130,7 @@ check_inodes(const char *file_path, int *inode_tables, int group_count){
     return NULL;
     // quando tiver os endereços das tabelas de inodes, passar por elas checkando falhas nos inodes
 }
+
 
 int main(int argc, char const *argv[])
 {
@@ -136,7 +151,7 @@ int main(int argc, char const *argv[])
 	
 	group_size = block_size * sb_backup->s_blocks_per_group;
 
-	find_inode_tables(argv[1], group_count);
+	find_inode_paths(argv[1], group_count);
 
 	free(sb_backup);
 
