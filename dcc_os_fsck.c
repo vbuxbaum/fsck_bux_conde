89eab7de385f88d54e9e2ad48c0d6c8d03a85bdc
Vitor buxbaum <vitorbuxbaum@gmail.com>
2017-11-08 22:27:06 -0200

    resolve ataques 1, 2 e 3

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -11,11 +11,6 @@ int _group_size;
 // calcula onde está o início do grupo i
 #define GROUP_OFFSET(i) (BASE_OFFSET + i*_group_size)
 
-
-int INODE_NO(int i, int j){
-	return (i*8 + j) +1;
-}
-
 // ----------------------------------------------------
 
 
@@ -86,10 +81,10 @@ check_superblock(const char *file_path){
 
     struct ext2_super_block *super = malloc(sizeof(struct ext2_super_block));
 
-    lseek(img_fd, BLOCK_OFFSET(0), SEEK_SET);
+    lseek(img_fd, BASE_OFFSET, SEEK_SET);
     read(img_fd, super, sizeof(struct ext2_super_block));
 
-    if (super->s_blocks_count == 0 || super->s_inode_size == 0) return NULL;
+    if (super->s_blocks_count == 0 || super->s_inode_size == 0) {return NULL;}
    
     return super;
 }
@@ -111,6 +106,124 @@ read_gdtable(const char *file_path, int group_count){
     return gd_table;
 }
 
+/* -------------------------------------------------------
+check_i_bitmap :
+	> retorna o INDICE do inode no bitmap, caso esteja ATIVO
+	> retorna ZERO se inode NÃO ESTIVER ATIVO 
+*/
+int 
+check_i_bitmap(int i_n, unsigned char *bitmap, int i_per_grp) {
+	int index = (i_n-1)%i_per_grp;
+
+	int i = index/8;
+	int j = index%8;
+
+	return (bitmap[i] & (1 << j)) ? index : 0;
+}
+
+struct ext2_inode *
+read_inode(int img_fd, int i_index, int i_table_blk ){
+	struct ext2_inode *inode = malloc(sizeof(struct ext2_inode)) ;
+
+	lseek(img_fd, BLOCK_OFFSET(i_table_blk) + i_index*sizeof(struct ext2_inode), SEEK_SET);
+	read(img_fd, inode, sizeof(struct ext2_inode));
+
+	return inode;
+} 
+
+void
+write_inode(int img_fd, struct ext2_inode *inode, int i_index, int i_table_blk ){
+	lseek(img_fd, BLOCK_OFFSET(i_table_blk) + i_index*sizeof(struct ext2_inode), SEEK_SET);
+	write(img_fd, inode, sizeof(struct ext2_inode));
+} 
+
+void 
+delete_inode(int img_fd, int index, unsigned char *bitmap, int i_bitmap_blk){
+	int i = index/8;
+	int j = index%8;
+
+	bitmap[i] |= !(1 << j);
+	
+	lseek(img_fd, BLOCK_OFFSET(i_bitmap_blk), SEEK_SET);     	// acha local do bitmap
+	write(img_fd, bitmap, _block_size); // escreve novo bitmap no arquivo
+
+}
+
+/* -------------------------------------------------------
+check_ind_blocks :
+	> retorna o numero do outro Inode, caso um bloco já esteja usado
+	> retorna ZERO se estiver TUDO OK 
+*/
+int 
+check_ind_blocks(int img_fd, int i_n, int level, int ind_block_addr, list *i_b_list, int *blk_cnt){
+	int links_per_block = _block_size/sizeof(int);
+	int *ind_block = malloc(_block_size);
+
+	lseek(img_fd, BLOCK_OFFSET(ind_block_addr),SEEK_SET);
+	read(img_fd, ind_block, _block_size);
+
+	for (int i = 0; i < links_per_block; ++i)
+	{
+		if (ind_block[i] == 0) { continue; }
+
+		if (level > 1) {
+			int result = check_ind_blocks(img_fd, i_n, level-1, ind_block[i], i_b_list, blk_cnt);
+			if (result)	{ return result; }
+				else	{ continue;	}
+		}
+
+		int blk_used = isBlockUsed(i_b_list, ind_block[i]);
+		if ((blk_used != (i_n)) && (blk_used != 0)) {// quando inode usa bloco já utilizado por outro inode
+			return blk_used;
+		} 
+
+		*blk_cnt ++;
+		insert(i_b_list, i_n, ind_block[i]);
+	}
+
+	return 0;
+}
+
+/* -------------------------------------------------------
+check_inode : 
+	> retorna o numero do inode se estiver tudo ok;
+	> retorna o numero do outro Inode, caso um bloco já esteja usado
+	> retorna ZERO se estiver sem permissões
+*/
+int
+check_inode(int img_fd, int i_n, struct ext2_inode *inode, list *i_b_list){
+	int blk_cnt = 0; 
+	int result = -1;
+
+	for (int i = 0; i < EXT2_N_BLOCKS && blk_cnt < inode->i_blocks ; ++i) {
+		
+		if (inode->i_block[i] == 0) { continue; }
+		
+		if (i == EXT2_IND_BLOCK) { 
+			result = check_ind_blocks(img_fd, i_n, 1, inode->i_block[i], i_b_list, &blk_cnt); 
+			continue;
+		} if (i == EXT2_DIND_BLOCK){
+			result = check_ind_blocks(img_fd, i_n, 2, inode->i_block[i], i_b_list, &blk_cnt);
+			continue;
+		} if (i == EXT2_TIND_BLOCK) {
+			result = check_ind_blocks(img_fd, i_n, 3, inode->i_block[i], i_b_list, &blk_cnt);
+			continue;
+		} 
+
+		int blk_used = isBlockUsed(i_b_list, inode->i_block[i]);
+			
+		if (blk_used != 0 && blk_used != i_n) { return blk_used; }
+			
+		blk_cnt ++;
+		insert(i_b_list, i_n, inode->i_block[i]);
+	}
+
+	if (result >= 0) { 
+		return (result > 0) ? result : i_n; 
+	}
+
+	return (inode->i_mode == 0) ? 0 : i_n;
+}
 
 /*
 
@@ -123,12 +236,12 @@ int main(int argc, char const *argv[])
 {
 	_block_size = 1024;
 
-	printf("\n> > > Conferindo Superbloco . . .\n\n");
+	printf("\n> > > Conferindo Superbloco . . .\n");
 
 	struct ext2_super_block *superblock = check_superblock(argv[1]);
 
 	if ( superblock == NULL ) {
-		printf("Erro na leitura do superbloco. \nDeseja recuperar backup? ('s' para sim)");
+		printf("> > Erro na leitura do superbloco. \nDeseja recuperar backup? ('s' para sim)");
 		char c_opt; scanf(" %c",&c_opt);
 		if (c_opt == 's'){
 			superblock = retrieve_sb_backup(argv[1]);
@@ -138,45 +251,102 @@ int main(int argc, char const *argv[])
 		} else {
 			printf(	"> > Superbloco corrompido MANTIDO!\n O FSCK não pode fazer mais nada... Adeus! \n.. Só love Só love ..\n\n");
 			free(superblock);
-			return 0;
+			return -1;
 		} // 
-	}
+	} else { printf("> OK!\n"); }
 	
 	int group_count = 1 + (superblock->s_blocks_count-1) / superblock->s_blocks_per_group;
 	//_group_size = _block_size * superblock->s_blocks_per_group;
 
-	printf("\n> > > Lendo Tabela de Descritores de Grupo . . .\n\n");
+	printf("\n> > > Lendo Tabela de Descritores de Grupo . . .\n");
 
 	struct ext2_group_desc *gd_table = read_gdtable(argv[1],group_count);
 
-	for (int i = 0; i < group_count; ++i)
-	{
-		struct ext2_group_desc gd = gd_table[i];
-
-		printf("%i\n%i\n%i\n%hu\n%hu\n%hu\n%hu\n ---------- \n",gd.bg_block_bitmap, gd.bg_inode_bitmap, gd.bg_inode_table, gd.bg_free_blocks_count,gd.bg_free_inodes_count,gd.bg_used_dirs_count, gd.bg_pad );
-	}
-
-	/*if (gd_table == NULL) {
-		printf(	"Erro na leitura da tabela de descritores de grupo. 
-				\n O FSCK não pode fazer mais nada... Adeus! \n.. Só love Só love ..\n\n");
+	if (gd_table == NULL) {
+		printf(	"> > Erro na leitura da tabela de descritores de grupo. \n O FSCK não pode fazer mais nada... Adeus! \n.. Só love Só love ..\n\n");
 		free(superblock); free(gd_table);
-		return 0;
-	} else {
+		return -1; // erro na leitura da tabela de descritores
+	} else { 
+		int img_fd;
+		if ((img_fd = open(argv[1], O_RDWR)) < 0){
+	        printf("Falha ao Abrir o arquivo %s.\n", argv[1]);
+	        return -1;
+	    }
+
+	    list i_b_list; // LISTA PARA AUXILIAR CHECKAGEM POR BLOCOS DE MAIS DE UM DONO
+    	newList(&i_b_list);
+
+    	list i_found_list; // LISTA PARA REGISTRAR OS INODES ENCONTRADOS NA VARREDURA
+    	newList(&i_found_list);
+
+    	printf("\n> > > Conferindo inodes . . .\n\n");
+
+	    printf("inode\tblocks\tsize\n");
 		
-		for (int i_n = super->s_first_ino; i < superblock->s_inodes_count; ++i_n) {
+		for (int i_n = superblock->s_first_ino; i_n < superblock->s_inodes_count; ++i_n) {
 
-			int grp = 
+			int grp = (i_n - 1)/superblock->s_inodes_per_group;
 
-			if (check_i_bitmap(i_n, &gd_table[grp])) {
-				struct ext2_inode *inode = 
-			}
+			unsigned char bitmap[_block_size];
 
-		}
+			lseek(img_fd, BLOCK_OFFSET(gd_table[grp].bg_inode_bitmap), SEEK_SET);
+			read(img_fd, bitmap, _block_size);
+
+			int index = check_i_bitmap(i_n, bitmap, superblock->s_inodes_per_group);
+
+			if ( index > 0) {
+				struct ext2_inode *inode = read_inode(img_fd, index, gd_table[grp].bg_inode_table);				
+				//printf("%i\t%i\t%i\n", i_n, inode->i_blocks, inode->i_size);
+
+				int check_result = check_inode(img_fd, i_n, inode, &i_b_list);
+
+				if ( check_result == i_n ){
+					printf("i_node %i ok\n", i_n);
+					insert(&i_found_list, i_n, 1);
+					// ADICIONAR À LISTA DE INODES ENCONTRADOS, COM BLOCO = 1 
+
+				} else if (check_result == 0) { 
+					printf("\n> > JINZU!! i_node %i tem PERMISSÃO INVÁLIDA.\n", i_n);
+					printf("    Qual permissão deseja aplicar? .. ");
+
+					short int mode_opt; scanf(" %hu",&mode_opt);
+
+					while (mode_opt <= 0){
+						printf("> Opção inválida... tente um número maior que zero. .. ");
+						scanf(" %hu", &mode_opt);
+					} 
+
+					inode->i_mode = mode_opt;
+
+					write_inode(img_fd, inode, index, gd_table[grp].bg_inode_table);
+
+				} else {
+					printf("\n> > OOPSIE!! i_node %i utiliza BLOCO DO INODE %i. \n", i_n, check_result);
+					printf("    Deseja apagar Inode %i? ('s' para 'sim')", i_n);
+					char c_opt; scanf(" %c",&c_opt);
+
+					if (c_opt == 's' || c_opt == 'S') { // remove inode da lista e do bitmap
+						delete_inode(img_fd, index  , bitmap , gd_table[grp].bg_inode_bitmap); 	// remove inode do bitmap e atualiza bitmap
+						removeInode(&i_b_list, i_n); 					// remove inode da lista
+
+						printf("> > Inode %i REMOVIDO\n\n", i_n);
+
+						insert(&i_found_list, i_n, -1);
+						// ADICIONAR À LISTA DE INODES ENCONTRADOS, COM BLOCO = -1 
+
+					} else printf("> > Inode mantido\n");
+
+				}
+
+			} else { /* inode zerado no bitmap */ }
+
+		} // FIM DO LOOP
+
+		// NOVO LOOP PARA VARREDURA DOS DIRETÓRIOS
+
+		
+
 	}
-*/
-	printf("\n> > > Conferindo INodes . . .\n\n");
-
-
 
 	return 0;
 }
